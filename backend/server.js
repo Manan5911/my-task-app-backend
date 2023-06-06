c2846b5cd4e1b3a708a0720626a9f5bef25c3138
Manan Mittal <mananmittal333@gmail.com>
2023-06-06 21:43:53 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const Task = require("./models/taskModel");
+const taskRoutes = require("./routes/taskRoute");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors({
+    origin: ["http://localhost:3000", "http://my-task-app.onrender.com"]
+}));
+
+app.use("/api/tasks",taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        await connectDB();        
+        app.listen(PORT, ()=>{
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
